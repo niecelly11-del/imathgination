568facbc7070cd312c8d05c7a2bd98a81e526beb
niecelly11-del <niecelly11@gmail.com>
2025-10-19 00:00:14 +0800

    codes

---
+++ role_data.js
@@ -0,0 +1,22 @@
+/* const USERS = [
+  { username: "admin", password: "1234", role: "admin" },
+  { username: "teacher", password: "1234", role: "teacher" },
+];
+
+const ROLE_ACCESS = {
+  admin: [
+    "Login Page",
+    "User Management Module",
+    "System Settings",
+    "Website Management Dashboard",
+    "Enrollment Page"
+  ],
+  teacher: [
+    "Login Page",
+    "Data Reports Module",
+    "Assessment Customization Module",
+    "Teacher Dashboard",
+    "View Progress Tracker"
+  ]
+};
+*/
